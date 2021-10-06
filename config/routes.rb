24b30a64820027fafb8a1e2350917f9977c9abdb
Arthur Ace <arthur4it@gmail.com>
2021-10-06 21:57:25 +0800

    add deploy step

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+	root to: 'math_powers#index'
   resources :math_powers
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
